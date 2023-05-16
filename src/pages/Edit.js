5bae22ffe8a47ce57c1eb6bcbbcdeaf8d2546083
grittychu <grittychu@gmail.com>
2023-05-16 23:37:24 +0900

    Routing

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
+//setSeartchParams는 searchParams를 변경시키는 역할을 한다.
+
 const Edit = () => {
-    return (
-        <div>
-            <h1>Edit</h1>
-            <p> 일기 수정 페이지 입니다. </p>
-        </div>
-    );
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id=", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode=", mode);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p> 일기 수정 페이지 입니다. </p>
+    </div>
+  );
 };
 
 export default Edit;
