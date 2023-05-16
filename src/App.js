baa040080b3888f30f0b77b576aaaee2e783da6f
grittychu <grittychu@gmail.com>
2023-05-16 21:34:29 +0900

    Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import "./App.css";
 import {BrowserRouter ,Route, Routes} from 'react-router-dom';
 
-//2_만들어 놓은 component들을 import
+import RouteTest from "./components/RouteTest";
+
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 
-//3_
 function App() {
   return (
     <BrowserRouter>
    <div className='App'>
     <h1>App.js</h1>
+    <p>Route 밖에 작성한 내용은 공통으로 보여지게 된다</p>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/new" element={<New/>}/>
       <Route path="/Diary" element={<Diary/>}/>
       <Route path="Edit" element={<Edit/>}/>
     </Routes>
+    <RouteTest/>
    </div>
    </BrowserRouter>
   );
