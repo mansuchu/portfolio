66af2cdebc66e73c4efa0d085dc5624d83ae32c2
mansuchu <grittychu@gmail.com>
2023-05-20 02:02:51 +0900

    img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,11 @@ const Home = () => {
         <div>
             <h1>Home</h1>
             <p> 홈 페이지 입니다. </p>
+
+            <img src="/img/Group 10.png" />
+            <img src="/img/Group 9.png" />
+            <img src="/img/Group 11.png" />
+            <img src="/img/Group 12.png" />
         </div>
     );
 };
