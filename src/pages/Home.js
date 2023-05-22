960ee20b5bd5b3d0d0ef50ad41639f1bd445896a
grittychu <grittychu@gmail.com>
2023-05-22 20:15:41 +0900

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,39 +26,41 @@ const Home = () => {
   return (
     <div className="firstDiv">
       <div className="parentDiv">
-        <img
-          src="/img/Group 12.png"
+        <div
           id="nonameImg"
-          alt="Image 1"
           onMouseOver={() => handleMouseOver("nonameImg")}
           onMouseOut={() => handleMouseOut("nonameImg")}
           style={{ transform: `translateY(${imageOffsets.nonameImg}px)` }}
-        />
-
-        <img
-          src="/img/Group 9.png"
+        >
+          <img src="/img/Group 12.png" alt="Image 1" />
+        </div>
+        <div
           id="skillsImg"
-          alt="Image 2"
           onMouseOver={() => handleMouseOver("skillsImg")}
           onMouseOut={() => handleMouseOut("skillsImg")}
           style={{ transform: `translateY(${imageOffsets.skillsImg}px)` }}
-        />
-        <img
-          src="/img/Group 10.png"
+        >
+          <img src="/img/Group 9.png" alt="Image 2" />
+        </div>
+        <div
           id="projectImg"
-          alt="Image 3"
           onMouseOver={() => handleMouseOver("projectImg")}
           onMouseOut={() => handleMouseOut("projectImg")}
           style={{ transform: `translateY(${imageOffsets.projectImg}px)` }}
-        />
-        <img
-          src="/img/Group 11.png"
+        >
+          <img src="/img/Group 10.png" alt="Image 3" />
+        </div>
+        <div
           id="AboutImg"
-          alt="Image 4"
           onMouseOver={() => handleMouseOver("AboutImg")}
           onMouseOut={() => handleMouseOut("AboutImg")}
           style={{ transform: `translateY(${imageOffsets.AboutImg}px)` }}
-        />
+        >
+          <img src="/img/Group 11.png" alt="Image 4" />
+          <img src="/img/my.png" id="myImg" alt="Image 5" />
+
+          <p>안녕하세요 신입개발자 추승희 입니다</p>
+        </div>
       </div>
     </div>
   );
